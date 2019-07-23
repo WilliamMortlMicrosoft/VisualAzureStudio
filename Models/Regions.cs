fc7841dead540fc2d0215edca238a76bb2bb3d86
schlammbad <schlammbad@hotmail.com>
2019-07-23 11:12:33 -0700

    Simplified region list to just US regions for demo.

--- Models/Regions.cs
+++ Models/Regions.cs
@@ -1,58 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace VisualAzureStudio.Models
+﻿namespace VisualAzureStudio.Models
 {
     public enum Regions
     {
-        AustraliaCentral,
-        AustraliaCentral2,
-        AustraliaEast,
-        AustraliaSoutheast,
-        BrazilSouth,
-        CanadaCentral,
-        CanadaEast,
-        CentralIndia,
         CentralUS,
-        ChinaEast,
-        ChinaEast2,
-        ChinaNorth,
-        ChinaNorth2,
-        EastAsia,
         EastUS,
         EastUS2,
-        FranceCentral,
-        FranceSouth,
-        GermanyCentral,
-        GermanyNortheast,
-        JapanEast,
-        JapanWest,
-        KoreaCentral,
-        KoreaSouth,
-        NormalEast,
         NorthCentralUS,
-        NorthEurope,
-        SouthAfricaNorth,
-        SouthAfricaWest,
         SouthCentralUS,
-        SouthIndia,
-        SoutheastAsia,
-        UAECentral,
-        UAENorth,
-        UKSouth,
-        UKWest,
-        USDoDCentral,
-        USDoDEast,
-        USGovArizona,
-        USGovIowa,
-        USGovTexas,
-        USGovVirgina,
-        WestCentral,
-        WestEurope,
-        WestIndia,
+        WestCentralUS,
         WestUS,
         WestUS2
     }
