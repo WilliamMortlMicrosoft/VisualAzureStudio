9c73f58ef8306399bee1f5adc7cb4a0a9c17b69f
schlammbad <schlammbad@hotmail.com>
2019-07-23 13:08:37 -0700

    Added some descriptions and comments for clarity.

--- Models/Components/ComponentBase.cs
+++ Models/Components/ComponentBase.cs
@@ -1,8 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Newtonsoft.Json;
+using System;
 using System.ComponentModel;
 using System.Drawing;
-using Newtonsoft.Json;
 
 namespace VisualAzureStudio.Models.Components
 {
@@ -11,24 +10,35 @@ namespace VisualAzureStudio.Models.Components
         [Browsable(false)]
         public Guid Id { get; set; } = Guid.NewGuid();
 
+        [Description("ID of the Azure resource group that will contain this component.")]
         public Guid ResourceGroupId { get; set; }
 
-        [Description("Name of this object.")]
+        [Description("Name of this component.")]
         public string Name { get; set; }
 
         [Browsable(false)]
         public Point Location { get; set; }
 
+        [Description("Azure region that will host this component.")]
         public Regions Region { get; set; }
 
+        /// <summary>
+        /// Gets the string description of the type. For use by the user interface.
+        /// </summary>
         [JsonIgnore]
         [Browsable(false)]
         public abstract string TypeDescription { get; }
 
+        /// <summary>
+        /// Gets the index of the image to use for this type. For use by the user interface.
+        /// </summary>
         [JsonIgnore]
         [Browsable(false)]
         public abstract int ImageIndex { get; }
 
+        /// <summary>
+        /// Gets the background color to use for this type. For use by the user interface.
+        /// </summary>
         [JsonIgnore]
         [Browsable(false)]
         public abstract Color BackColor { get; }
