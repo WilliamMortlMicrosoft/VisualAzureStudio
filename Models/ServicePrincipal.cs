2d25bc411eb7839da7eb62e04aa1cc05c357c919
schlammbad <schlammbad@hotmail.com>
2019-07-24 10:33:43 -0700

    Added initial empty values for Name and Password.

--- Models/ServicePrincipal.cs
+++ Models/ServicePrincipal.cs
@@ -10,10 +10,10 @@ namespace VisualAzureStudio.Models
         public Guid Id { get; set; }
 
         [Description("Name of this service principal.")]
-        public string Name { get; set; }
+        public string Name { get; set; } = string.Empty;
 
         [Description("Password/client secret for this service principal.")]
-        public string Password { get; set; }
+        public string Password { get; set; } = string.Empty;
 
         [Description("ID of the Azure tenant containing this service principal.")]
         public Guid TenantId { get; set; }
