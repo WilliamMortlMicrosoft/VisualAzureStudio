8eb8fc512e80d9593889d75e467c16d83ee7763a
schlammbad <schlammbad@hotmail.com>
2019-07-24 11:56:30 -0700

    Removed unused Secrets property.

--- Models/Components/KeyVault.cs
+++ Models/Components/KeyVault.cs
@@ -1,11 +1,9 @@
-﻿using System.Collections.Generic;
-using System.Drawing;
+﻿using System.Drawing;
 
 namespace VisualAzureStudio.Models.Components
 {
     public class KeyVault : ComponentBase
     {
-        public Dictionary<string, string> Secrets { get; set; } = new Dictionary<string, string>();
         public override string TypeDescription => "Key Vault";
         public override int ImageIndex => 1;
         public override Color BackColor => Color.FromArgb(255, 255, 247, 219);
